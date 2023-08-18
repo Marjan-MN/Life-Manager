afbb6a320e6c245323794ca9eae951118074a0f3
Joel Getzke <joelcode2023@gmail.com>
2023-08-17 19:49:31 -0500

    date helper

---
+++ utils/helpers.js
@@ -0,0 +1,11 @@
+// getting dates
+function formatDate(date) {
+    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+        date
+      ).getFullYear()}`;
+}
+
+
+module.exports = {
+    formatDate
+}
