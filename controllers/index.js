97897b4ab6a3d0e57b601cd4c560454b37b869c2
Joel Getzke <joelcode2023@gmail.com>
2023-08-19 20:03:55 -0500

    save functionality done

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,20 @@ const homeRoutes = require('./homeRoutes');
 
 
 router.use('/', homeRoutes);
+// includes:
+// GET localhost:3001/tasks/
+
 router.use('/api', apiRoutes);
+// localhost:3001/api/user/* =>
+// POST localhost:3001/api/user/
+// POST localhost:3001/api/user/login
+// POST localhost:3001/api/user/logout
+
+// localhost:3001/api/tasks/* =>
+// GET localhost:3001/api/tasks/:id
+// POST localhost:3001/api/tasks/
+
+
 router.use('/dashboard', dashboard);
 
 module.exports = router;
