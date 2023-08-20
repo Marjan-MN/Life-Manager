f51cb4de3efdec345990140e446bebf0758c4045
Marjan-MN <131532495+Marjan-MN@users.noreply.github.com>
2023-08-20 17:51:13 -0500

    updated code

--- models/index.js
+++ models/index.js
@@ -1,30 +1,13 @@
 const User =  require('./user');
 const Tasks = require('./Tasks');
-const Tasklist = require('./Tasklist');
 // const Interest = require('./Interest');
 
+User.hasMany(Tasks,{
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 Tasks.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
-User.hasMany(Tasks,{
-  foreignKey: 'user_id'
-});
-
-// Interest.belongsToMany(User);
-
-// User.belongsToMany(Interest);
-
-Tasklist.belongsTo(User, {
-   foreignKey: 'user_id'
-});
-   
-
-Tasks.belongsTo(Tasklist, {
-  foreignKey: 'task_id'
-});
-
-
-
-
-module.exports = { User, Tasks, Tasklist};
+module.exports = { User, Tasks };
